fa70b7ce3dd87b32d07b5fdc7f75c00c07b80963
joshjun0122 <josh989870@gmail.com>
2024-12-03 08:58:46 +0800

    first commit

---
+++ P10-7/work/work/work.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+/* card structure definition */
+struct card {
+   const char *face; /* define pointer face */   //使用const 只是因為警告 理論上是可以的
+   const char *suit; /* define pointer suit */
+}; /* end structure card */
+
+int main(void)
+{
+    struct card aCard; /* define one struct card variable */
+    struct card *cardPtr; /* define a pointer to a struct card */
+
+    /* place strings into aCard */
+    aCard.face = "Ace";
+    aCard.suit = "Spades";
+
+    cardPtr = &aCard; /* assign address of aCard to cardPtr */
+
+    printf("%s%s%s\n%s%s%s\n%s%s%s\n",aCard.face, " of ", aCard.suit, cardPtr->face, " of ",cardPtr->suit, (*cardPtr).face, " of ", (*cardPtr).suit);  //三種寫法都可以
+
+    return 0; /* indicates successful termination */
+} /* end main */
---
+++ P10-9/work/work/work.cpp
@@ -0,0 +1,57 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* card structure definition */
+typedef struct card {
+    const char* face; /* define pointer face */   //使用const 只是因為警告 理論上是可以的
+    const char* suit; /* define pointer suit */
+}Card; /* end structure card */
+
+void fillDeck(Card* const wDeck, const char* wFace[], const char* wSuit[]);
+void shuffle(Card* const wDeck);
+void deal(const Card* const wDeck);
+
+
+int main(void)
+{
+    Card deck[52];
+
+    const char* face[] = { "Ace","Deuce","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King" };
+    const char* suit[] = { "Hearts","Diamonds","Clubs","Spades" };
+
+    srand(time(NULL));
+
+    fillDeck(deck, face, suit);
+    shuffle(deck);
+    deal(deck);
+
+    return 0; /* indicates successful termination */
+} /* end main */
+
+void fillDeck(Card* const wDeck, const char* wFace[], const char* wSuit[]) {     //分配函數
+    int i;
+    for (i = 0;i <= 51;i++) {
+        wDeck[i].face = wFace[i % 13];   // 給數字
+        wDeck[i].suit = wSuit[i / 13];   // 給符號
+    }
+};
+
+void shuffle(Card* const wDeck) {     //隨機數交換陣列中的卡片
+    int i;
+    int j;
+    Card temp;
+    for (i = 0;i <= 51;i++) {
+        j = rand() % 52;
+        temp = wDeck[i];
+        wDeck[i] = wDeck[j];
+        wDeck[j] = temp;
+    }
+};
+
+void deal(const Card* const wDeck) {  //印出
+    int i;
+    for (i = 0;i <= 51;i++) {
+        printf("%5s of %-8s%s", wDeck[i].face, wDeck[i].suit, (i + 1) % 4 ? " " : "\n");      // 數字,符號,沒到四空格否則換行
+    }
+};
